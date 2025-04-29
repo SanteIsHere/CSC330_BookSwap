6cdad0ab941c6e33b25785a5c4e6ee532cff8e4c
SanteIsHere <r.a.frye00@gmail.com>
2025-04-29 17:54:55 -0400

    Add comments to `run.py` script

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+# Import app initialization function from `app/__init__.py`
 from app import create_app
 
+# Initialize the Flask application
 app = create_app()
 
+
 if __name__ == "__main__":
+    # Run the application in debug mode
     app.run(debug=True)
