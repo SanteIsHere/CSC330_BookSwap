661d48ad249c47fbd651fef6431046b91376026c
Orlando Marin <orlandomarin@orlandos-air.lan>
2025-05-03 03:04:20 -0400

    Build out create_listing page with new form fields and layout

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
-from .forms import LoginForm, RegisterForm
+from .forms import LoginForm, RegisterForm, CreateListingForm
 from .models import User
 from . import db
 
@@ -71,15 +71,37 @@ def profile():
     return render_template('profile.html')
 
 
+# ORLANDO
+# Create listing route for the user to post a listing
+@bp.route('/create_listing', methods=['GET', 'POST'])
+def create_listing():
+    # Create a new instance of the CreateListingForm
+    form = CreateListingForm()
+
+    # If the form is submitted and the data is valid
+    if form.validate_on_submit():
+        # Right now we’re not saving to the database yet
+        # Just showing a success message so we can test the form
+        flash("Listing submitted! (Not yet saved to the database)")
+        return redirect(url_for('main.create_listing'))
+
+    # Show the create_listing page with the form
+    return render_template('create_listing.html', form=form)
+
+
 @bp.route('/listings')
 def listings_view():
     # TODO: Implement this route
     pass
 
 
-@bp.route('/create_listing')
-def create_listing():
-    # TODO: Implement this route
-    pass
+# ORLANDO
+# Route to log the user out
+@bp.route('/logout')
+@login_required  # Only logged-in users should be able to log out
+def logout():
+    logout_user()  # End the user’s session
+    flash("You have been logged out.")  # Optional message shown on next page
+    return redirect(url_for('main.welcome'))  # Send them back to the welcome page
 
-# TODO: Implement logout route and functionality
+
