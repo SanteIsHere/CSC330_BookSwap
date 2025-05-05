71e9ed62eb292e0c71db327cc3279e4b60e0c6b2
SanteIsHere <r.a.frye00@gmail.com>
2025-05-05 19:49:30 -0400

    Fix merge conflict in `routes.py`

--- app/routes.py
+++ app/routes.py
@@ -92,23 +92,6 @@ def create_listing():
     return render_template('create_listing.html', form=form)
 
 
-# ORLANDO
-# Create listing route for the user to post a listing
-@bp.route('/create_listing', methods=['GET', 'POST'])
-def create_listing():
-    # Create a new instance of the CreateListingForm
-    form = CreateListingForm()
-
-    # If the form is submitted and the data is valid
-    if form.validate_on_submit():
-        # Right now we’re not saving to the database yet
-        # Just showing a success message so we can test the form
-        flash("Listing submitted! (Not yet saved to the database)")
-        return redirect(url_for('main.listings_view'))
-
-    # Show the create_listing page with the form
-    return render_template('create_listing.html', form=form)
-
 # TATIANA
 # Route to view listings
 @bp.route('/listings')
@@ -141,11 +124,12 @@ def view_listings():
 
 # ORLANDO
 # Route to log the user out
+
+
 @bp.route('/logout')
 @login_required  # Only logged-in users should be able to log out
 def logout():
     logout_user()  # End the user’s session
     flash("You have been logged out.")  # Optional message shown on next page
-    return redirect(url_for('main.welcome'))  # Send them back to the welcome page
-
-
+    # Send them back to the welcome page
+    return redirect(url_for('main.welcome'))
